d030ff6e255b5e1477680dfae8e166d18d7c1cb4
test <yassine6996@gmail.com>
2023-12-03 22:12:08 +0100

    first commit

---
+++ billing/src/main/java/org/sid/billing/ConsulConfigRestController.java
@@ -0,0 +1,30 @@
+package org.sid.billing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+//@RefreshScope
+public class ConsulConfigRestController {
+    @Autowired
+    private MyConsulConfig myConsulConfig;
+    @Autowired
+    private MyVaultConfig myVaultConfig;
+    //    @Value("${token.accessTokenTimeout}")
+//    private long accessTokenTimeout;
+//    @Value("${token.refreshTokenTimeout}")
+//    private long refreshTokenTimeout;
+    @GetMapping("/myConfig")
+    public Map<String, Object> getConfig(){
+        return Map.of("consulConfig",myConsulConfig,"myVaultConfig",myVaultConfig);
+    }
+
+//    public MyConsulConfig myConfig(){
+//        return myConsulConfig;//Map.of("accessTokenTimeout",accessTokenTimeout,"refreshTokenTimeout",refreshTokenTimeout);
+//    }
+
+}
